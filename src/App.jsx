f94e45534a2c13bdb7c6b6cdeb69bbc8d24c719a
Kenny <kenny@Kennys-MacBook-Air.local>
2025-04-06 22:53:01 -0400

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import rocketLogo from 'Green_left-facing_rocket.png'
 import './App.css'
 
 function App() {
@@ -9,14 +10,21 @@ function App() {
   return (
     <>
       <div>
+        {/*
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        */}
+        <a href="https://ketel.ai" target="_blank">
+          <img src={rocketLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Ketel AI's mission is to make implementation better, faster, and easier.</h1>
+      {/*
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,8 +33,15 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      */}
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Contact Us
+      </p>
+      <p className="dylan-contact">
+        Dylan Patel dylan@ketel.ai
+      </p>
+      <p className="kenny-contact">
+        Kenny Oen kenny@ketel.ai
       </p>
     </>
   )
